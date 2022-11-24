1c25e2ee2a1f80bf0e4159a50e4a00b8709ca7f2
Savas Vedova <savas@stormkit.io>
2022-11-24 13:10:23 +0300

    chore: display the name parameter

--- src/pages/[name]/index.tsx
+++ src/pages/[name]/index.tsx
@@ -1,9 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const UserHome: React.FC = () => {
+  const params = useParams<{ name: string }>();
+
   return (
     <div>
       <p>Server side rendered user home page.</p>
+      <p>Your name is {params.name}</p>
       <Link to="/">Home</Link>
     </div>
   );
