a9aea22de4ee3d560c9a89056d90f1111cf8270e
Savas Vedova <savas@stormkit.io>
2022-12-30 20:10:34 +0300

    chore: update signature

--- src/api/subscribe.post.ts
+++ src/api/subscribe.post.ts
@@ -4,14 +4,16 @@ import { readBody, send } from "~/helpers/api/http";
 import { WAITING_LIST } from "~/helpers/api/collections";
 
 export default async (req: http.IncomingMessage, res: http.ServerResponse) => {
-  const { email } = await readBody<{ email?: string }>(req);
+  const { email } = (await readBody<{ email?: string }>(req)) || {};
 
   try {
     const records = await ds.store(WAITING_LIST, { email });
     send(res, { records });
-  } catch ({ response }) {
-    // @ts-ignore
-    if (response.status === 409) {
+  } catch (response: unknown) {
+    if (
+      response instanceof http.IncomingMessage &&
+      response.statusCode === 409
+    ) {
       res.statusCode = 409;
       send(res, { error: "duplicate" });
     } else {
