128df2f390d3de9a9ca83b657f3907fb9ea3b2d0
Savas Vedova <savas@stormkit.io>
2022-12-30 20:11:23 +0300

    chore: bump stormkit serverless package

--- src/api/subscribe.post.ts
+++ src/api/subscribe.post.ts
@@ -4,10 +4,16 @@ import { readBody, send } from "~/helpers/api/http";
 import { WAITING_LIST } from "~/helpers/api/collections";
 
 export default async (req: http.IncomingMessage, res: http.ServerResponse) => {
+  console.log("parsing request");
+
   const { email } = (await readBody<{ email?: string }>(req)) || {};
 
+  console.log("request parsed:", email);
+
   try {
+    console.log("storing data");
     const records = await ds.store(WAITING_LIST, { email });
+    console.log("data store complete");
     send(res, { records });
   } catch (response: unknown) {
     if (
