bc09d54d23a59fd80dfa226efd0e1a855ab5033f
Savas Vedova <savas@stormkit.io>
2023-01-02 18:31:59 +0300

    chore: update home page design
    - How it works section
    - Tiny redesign

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,7 +1,18 @@
+import sklogo from "~/assets/stormkit-logo.svg";
+
 const Footer: React.FC = () => {
   return (
-    <footer className="p-4">
-      <div className="max-w-screen-lg m-auto">Feez</div>
+    <footer className="p-4 text-center">
+      <div className="max-w-screen-lg m-auto flex items-center justify-center">
+        Powered by{" "}
+        <a
+          href="https://www.stormkit.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={sklogo} alt="Stormkit" className="w-24 ml-1" />
+        </a>
+      </div>
     </footer>
   );
 };
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,7 +1,14 @@
+import logo from "~/assets/logo-text.svg";
+import { Link } from "react-router-dom";
+
 const Header: React.FC = () => {
   return (
-    <header className="p-4">
-      <div className="max-w-screen-lg m-auto">Feez</div>
+    <header className="p-4 bg-indigo-900 bg-opacity-30">
+      <div className="max-w-screen-lg m-auto leading-none">
+        <Link to="/" className="inline-flex">
+          <img src={logo} alt="Feez.ws" className="w-24" />
+        </Link>
+      </div>
     </header>
   );
 };
